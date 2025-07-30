291e987ccc2f9e9d95850324adb1efb69d41e74e
Alan Santana <alansantanavieira@gmail.com>
2025-07-29 23:07:56 -0300

    back end

---
+++ app/Http/Controllers/DotmeController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\FormRequestCadastro;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Models\Dotme;
+
+class DotmeController extends Controller
+{
+    public function create(FormRequestCadastro $request){
+        if($request->method() == "POST"){
+            $data = $request->all();
+
+            Dotme::create($data);
+
+            return redirect('log');
+        }
+
+        return view('log');
+    }
+
+    public function login(LoginRequest $request)
+    {
+        if ($request->isMethod('post')) {
+            $data = $request->all();
+
+            $user = Dotme::where('email', $data['email'])->first();
+
+            if ($user === null) {
+                return redirect()->back()->with('error', 'Usuário não encontrado');
+            }
+
+           //aqui chamar a autenticação pra validar a senha
+           return view('index');
+        }  
+    }
+}
